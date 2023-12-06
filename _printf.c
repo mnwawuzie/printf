ebcc1b4f44a10ea8e0aad2943a25b906e7785495
mnwawuzie <chinazamaryann19@gmail.com>
2023-12-05 16:04:13 -0800

    delete all

--- _printf.c
+++ _printf.c
@@ -1,45 +1,56 @@
 #include "main.h"
+
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - function that output according to a format
+ * @format: a character string
+ *
+ * Return: the number of characters printed
  */
-int _printf(const char * const format, ...)
+
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
+        int num_of_bytes, string_arg_len, i, j = 0;
+        char nextChar;
 
-	va_list args;
-	int i = 0, j, len = 0;
+        va_list args;
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+        va_start(args, format);
 
-Here:
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
-	va_end(args);
-	return (len);
+        for (i = 0; format[i] != '\0'; i++)
+        {
+                if (format[i] == '%')
+                {
+                nextChar = format[i + 1];
+                num_of_bytes++;
+                i++;
+
+                        if (nextChar == 'c')
+                        {
+                                _putchar(va_arg(args, int));
+                        }
+                        else if (nextChar == 's')
+                        {
+                                char string_arg = va_arg(args, char);
+
+                                string_arg_len = strlen(string_arg);
+
+                                for ( j = 0; j < string_arg_len; j++)
+                                {
+                                        _putchar(string_arg[j]);
+                                }
+                                num_of_bytes = num_of_bytes + string_arg_len - 1;
+                        }
+                        else if (nextChar == '%')
+                        {
+                                _putchar(format[i]);
+                        }
+                }
+                else
+                {
+                        _putchar(format[i]);
+                        num_of_bytes++;
+                }
+        }
+        va_end(args);
+        return (num_of_bytes);
 }
--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - print a charactor to std.
+ * @c: charactor to print.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+ * Return: 1 on sucess
+*/
+
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+        return (write(1, &c, 1));
 }
