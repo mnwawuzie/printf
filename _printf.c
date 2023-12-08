21c8def71c446535c5e44737ec24fee605c22f51
mthethonhla <nhlamthetho@outlook.com>
2023-12-08 07:04:18 -0800

    2nd _printf

--- _printf.c
+++ _printf.c
@@ -9,48 +9,56 @@
 
 int _printf(const char *format, ...)
 {
-	int num_of_bytes, string_arg_len, i, j;
 	char nextChar;
+
+	int i, num_chars, bytes_printed, string_len = 0;
 
 	va_list args;
 
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; i < format[i]; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] == '\t' || format[i] == '\n')
 		{
-		nextChar = format[i + 1];
-		num_of_bytes++;
-		i++;
+		bytes_printed--;
+		}
+		else if (format[i] == '%')
+		{
+			nextChar = format[i + 1];
 
-			if (nextChar == 'c')
-			{
-				_putchar(va_arg(args, int));
-			}
-			else if (nextChar == 's')
+			if (nextChar == 's')
 			{
 				char *string_arg = va_arg(args, char*);
 
-				string_arg_len = (int)strlen(string_arg);
-
-				for (j = 0; j < string_arg_len; j++)
+				while (string_arg[string_len])
 				{
-					_putchar(string_arg[j]);
+					write(1, &string_arg[string_len], 1);
+					string_len++;
 				}
-				num_of_bytes = num_of_bytes + string_arg_len - 1;
+				break;
+			}
+			else if (nextChar == 'c')
+			{
+				_putchar(va_arg(args, int));
+				num_chars++;
 			}
 			else if (nextChar == '%')
 			{
 				_putchar(format[i]);
+				num_chars++;
 			}
 		}
 		else
 		{
-			_putchar(format[i]);
-			num_of_bytes++;
+		num_chars++;
+		bytes_printed = bytes_printed + num_chars;
+		_putchar(format[i]);
 		}
+
 	}
 	va_end(args);
-	return (num_of_bytes);
+	bytes_printed = string_len + num_chars;
+	return (bytes_printed);
+
 }
