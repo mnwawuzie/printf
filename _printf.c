55512491e1b9ee22287b6a1552678849f19918dc
mthethonhla <nhlamthetho@outlook.com>
2023-12-08 08:12:42 -0800

    back to first commit

--- _printf.c
+++ _printf.c
@@ -1,64 +1,56 @@
 #include "main.h"
 
 /**
- * _printf - prints output according to format
- * @format: format specifier
- * Return: the number of characters
+ * _printf - function that output according to a format
+ * @format: a character string
  *
+ * Return: the number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	char nextChar;
+        int num_of_bytes, string_arg_len, i, j = 0;
+        char nextChar;
 
-	int i, num_chars, bytes_printed, string_len = 0;
+        va_list args;
 
-	va_list args;
+        va_start(args, format);
 
-	va_start(args, format);
+        for (i = 0; format[i] != '\0'; i++)
+        {
+                if (format[i] == '%')
+                {
+                nextChar = format[i + 1];
+                num_of_bytes++;
+                i++;
 
-	for (i = 0; i < format[i]; i++)
-	{
-		if (format[i] == '\t' || format[i] == '\n')
-		{
-		bytes_printed--;
-		}
-		else if (format[i] == '%')
-		{
-			nextChar = format[i + 1];
+                        if (nextChar == 'c')
+                        {
+                                _putchar(va_arg(args, int));
+                        }
+                        else if (nextChar == 's')
+                        {
+                                char *string_arg = va_arg(args, char*);
 
-			if (nextChar == 's')
-			{
-				char *string_arg = va_arg(args, char*);
+                                string_arg_len = strlen(string_arg);
 
-				while (string_arg[string_len])
-				{
-					write(1, &string_arg[string_len], 1);
-					string_len++;
-				}
-				break;
-			}
-			else if (nextChar == 'c')
-			{
-				_putchar(va_arg(args, int));
-				num_chars++;
-			}
-			else if (nextChar == '%')
-			{
-				_putchar(format[i]);
-				num_chars++;
-			}
-		}
-		else
-		{
-		num_chars++;
-		bytes_printed = bytes_printed + num_chars;
-		_putchar(format[i]);
-		}
-
-	}
-	va_end(args);
-	bytes_printed = string_len + num_chars;
-	return (bytes_printed);
-
+                                for ( j = 0; j < string_arg_len; j++)
+                                {
+                                        _putchar(string_arg[j]);
+                                }
+                                num_of_bytes = num_of_bytes + string_arg_len - 1;
+                        }
+                        else if (nextChar == '%')
+                        {
+                                _putchar(format[i]);
+                        }
+                }
+                else
+                {
+                        _putchar(format[i]);
+                        num_of_bytes++;
+                }
+        }
+        va_end(args);
+        return (num_of_bytes);
 }
